ab83f10b76ad9901746f4a377501df5fafd04fae
Jacorey Rowe <jrowe@student.neumont.edu>
2023-10-03 22:50:20 -0600

    Spins on Q

--- Source/Application/World01.cpp
+++ Source/Application/World01.cpp
@@ -7,10 +7,6 @@ namespace nc
 {
     bool World01::Initialize()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            m_position.push_back({ randomf(-1,1), randomf(-1,1) });
-        }
         return true;
     }
 
@@ -21,8 +17,15 @@ namespace nc
     void World01::Update(float dt)
     {
         m_angle += 90 * dt;
+        
+        m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -dt : 0;
+        m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? +dt : 0;
         m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -dt : 0;
         m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? +dt : 0;
+        if (ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_Q))
+        {
+            glRotatef(m_angle, 0, 1, 0);
+        }
         m_time += dt;
 
     }
@@ -35,8 +38,9 @@ namespace nc
         // render
         glPushMatrix();
         glTranslatef(m_position.x,m_position.y,0);
-        //glRotatef(m_angle, 0, 1, 0);
-        glScalef((sin(m_time * 5) + 1) * 0.5f, 1, 1);
+        //TIME BASED TRANSFORMS
+        glRotatef(m_angle, 0, 1, 0);
+        //glScalef((sin(m_time * 5) + 1) * 0.5f, 1, 1);
 
         glBegin(GL_TRIANGLES); 
 
